20c0283aafbd00b19718358f255216a0803b8904
unknown <Dima>
2024-01-09 23:30:17 +0200

    End

--- src/index.js
+++ src/index.js
@@ -18,35 +18,71 @@ function App(){
 
     let [steps, setSteps] = useState(0);
 
+    let [isEnd, setIsEnd] = useState(false);
+
+    let [txt, setTxt] = useState("");
+
     function pc(){
-        if((!myStep) && (steps < 9)){
-            setSteps(steps + 1);
-            let id = Math.round(Math.random() * 8);
-            if(field[id].state == undefined){
-                let arr = field.map((el) => {
-                    if(el == field[id]){
-                        return {state:"x", id: el.id}
+            if((!myStep) && (steps < 9)){
+                if(!isEnd){
+                    setSteps(steps + 1);
+                    let id = Math.round(Math.random() * 8);
+                    if(field[id].state == undefined){
+                        let arr = field.map((el) => {
+                            if(el == field[id]){
+                                return {state:"x", id: el.id}
+                            }
+                            else{
+                                return el;
+                            }
+                        })
+                        setField(arr);
                     }
                     else{
-                        return el;
+                        pc();
                     }
-                })
-                setField(arr);
+                    setMyStep(true);
+                }
+                }
+    }
+
+    function win(f, s, t){
+        if((field[f].state == field[s].state) && (field[f].state == field[t].state) && (field[s].state == field[t].state) && (field[f].state !== undefined)){
+            if(field[f].state == "○"){
+                setTxt("You win!");
+                setSteps(9);
+                setIsEnd(true);
             }
-            else{
-                pc();
+            else if(field[f].state == "x"){
+                setTxt("You lose!");
+                setSteps(9);
+                setIsEnd(true);
             }
-            setMyStep(true);
         }
     }
 
+    function check(){
+        win(0,1,2)
+        win(3,4,5)
+        win(6,7,8)
+        win(0,3,6)
+        win(1,4,7)
+        win(2,5,8)
+        win(0,4,8)
+        win(2,4,6)
+    }
+
     useEffect(() => {
+        check();
         setTimeout(pc, 500);
     }, [myStep])
 
     useEffect(() => {
-        if(steps == 9){
-            setMyStep("-");
+        console.log(steps);
+        if(!isEnd){
+            if(steps == 9){
+                setMyStep(undefined);
+            }
         }
     }, [steps])
 
@@ -57,26 +93,30 @@ function App(){
                 <div className={style.field}>
                     {field.map((cell) => (
                         <div key={cell.id} className={style.cell} onClick={() => {
-                            if(myStep){
-                                if(cell.state == undefined){
-                                    setMyStep(false);
-                                    setSteps(steps + 1);
-                                    let arr = field.map((el) => {
-                                        if(el == cell){
-                                            return {state:"○", id: el.id};
-                                        }
-                                        else{
-                                            return el;
-                                        }
-                                    })
-                                    setField(arr);
-                                }
+                            if(!isEnd){
+                                if(myStep){
+                                    if(cell.state == undefined){
+                                        setMyStep(false);
+                                        setSteps(steps + 1);
+                                        let arr = field.map((el) => {
+                                            if(el == cell){
+                                                return {state:"○", id: el.id};
+                                            }
+                                            else{
+                                                return el;
+                                            }
+                                        })
+                                        check();
+                                        setField(arr);
+                                    }
+                                }   
                             }
                         }}>
                             {cell.state}
                         </div>
                     ))}
                 </div>
+                <h1>{txt}</h1>
             </div>
         </div>
     )
