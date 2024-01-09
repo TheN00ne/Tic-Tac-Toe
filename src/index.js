23a2c2f95a2f86c51ce059339dff221a4c1ed023
unknown <Dima>
2024-01-09 21:12:15 +0200

    playing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import style from "./style.module.css";
 
 let app = document.getElementById("app");
 
-let field = [
-    {state:undefined, id:1}, {state:undefined, id:2}, {state:undefined, id:3},
-    {state:undefined, id:4}, {state:undefined, id:5}, {state:undefined, id:6},
-    {state:undefined, id:7}, {state:undefined, id:8}, {state:undefined, id:9}
-]
-
 function App(){
+
+    let [field, setField] = useState([
+        {state:undefined, id:1}, {state:undefined, id:2}, {state:undefined, id:3},
+        {state:undefined, id:4}, {state:undefined, id:5}, {state:undefined, id:6},
+        {state:undefined, id:7}, {state:undefined, id:8}, {state:undefined, id:9}
+    ]);
+
+    let [myStep, setMyStep] = useState(true);
+
+    let [steps, setSteps] = useState(0);
+
+    function pc(){
+        if((!myStep) && (steps < 9)){
+            setSteps(steps + 1);
+            let id = Math.round(Math.random() * 8);
+            if(field[id].state == undefined){
+                let arr = field.map((el) => {
+                    if(el == field[id]){
+                        return {state:"x", id: el.id}
+                    }
+                    else{
+                        return el;
+                    }
+                })
+                setField(arr);
+            }
+            else{
+                pc();
+            }
+            setMyStep(true);
+        }
+    }
+
+    useEffect(() => {
+        setTimeout(pc, 500);
+    }, [myStep])
+
+    useEffect(() => {
+        if(steps == 9){
+            setMyStep("-");
+        }
+    }, [steps])
+
     return(
         <div>
             <h1>Tic-Tac-Toe</h1>
             <div className={style.outside}>
                 <div className={style.field}>
                     {field.map((cell) => (
-                        <div key={cell.id} className={style.cell}>
+                        <div key={cell.id} className={style.cell} onClick={() => {
+                            if(myStep){
+                                if(cell.state == undefined){
+                                    setMyStep(false);
+                                    setSteps(steps + 1);
+                                    let arr = field.map((el) => {
+                                        if(el == cell){
+                                            return {state:"○", id: el.id};
+                                        }
+                                        else{
+                                            return el;
+                                        }
+                                    })
+                                    setField(arr);
+                                }
+                            }
+                        }}>
                             {cell.state}
                         </div>
                     ))}
@@ -29,4 +82,4 @@ function App(){
     )
 }
 
-ReactDOM.render(App(), app)
+ReactDOM.render(<App/>, app);
