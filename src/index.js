a769523e72037c079270fb605de63904c93cfc9c
unknown <Dima>
2024-01-09 19:16:18 +0200

    Field

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import style from "./style.module.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let app = document.getElementById("app");
+
+let field = [
+    {state:undefined, id:1}, {state:undefined, id:2}, {state:undefined, id:3},
+    {state:undefined, id:4}, {state:undefined, id:5}, {state:undefined, id:6},
+    {state:undefined, id:7}, {state:undefined, id:8}, {state:undefined, id:9}
+]
+
+function App(){
+    return(
+        <div>
+            <h1>Tic-Tac-Toe</h1>
+            <div className={style.outside}>
+                <div className={style.field}>
+                    {field.map((cell) => (
+                        <div key={cell.id} className={style.cell}>
+                            {cell.state}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(App(), app)
